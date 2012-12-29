97b5870522b052ff81b54ee42f25f09f06b538ab
Haggai Philip Zagury <hagzag@gmail.com>
2012-12-30 00:45:15 +0200

    Minor refactoring

--- setup.sh
+++ setup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+__ruby_only() {
+        pushd ./ruby_via_rvm && ./install-ruby-rvm.sh; popd &>/dev/null
+}
+
+__chef_server() {
+        pushd ./chef_server && ./install_chef_server.sh; popd &>/dev/null
+}
+
 case "$1" in
-  ruby_only)
-        pushd ./ruby_via_rvm && ./install-ruby-rvm.sh; popd
+  ruby)
+	__ruby_only
         ;;
-  chef_server_only)
-        pushd ./chef_server && ./install_chef_server.sh; popd
+  chef_server)
+	__chef_server
         ;;
   all)
-        pushd ./ruby_via_rvm && ./install-ruby-rvm.sh; popd
-        pushd ./chef_server && ./install_chef_server.sh; popd
+	__ruby_only
+	__chef_server
         ;;
   git)
         . common.sh
         gitStuff
         ;;
   *)
-        echo $"Usage: $0 {ruby_only | chef_server_only | all | <internal usage: git>}"
+        echo $"Usage: $0 {ruby | chef_server | all | <internal usage: git>}"
         exit 1
         ;;
 esac

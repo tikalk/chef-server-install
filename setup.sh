2fe9fc1be1f14b329c8c87e9e11c110312cfdd26
Haggai Philip Zagury <hagzag@gmail.com>
2012-12-29 06:36:42 +0200

    Change reference in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ case "$1" in
         pushd ./ruby_via_rvm && ./install-ruby-rvm.sh; popd
         ;;
   chef_server_only)
-	pushd ./chef_server && ./install_chef_solo.sh; popd
+	pushd ./chef_server && ./install_chef_server.sh; popd
 	;;
   usage)
 	echo $"Usage: $0 {ruby_only}"
@@ -13,7 +13,7 @@ case "$1" in
 	;;	
   *)
 	pushd ./ruby_via_rvm && ./install-ruby-rvm.sh; popd
-	pushd ./chef_server && ./install_chef_solo.sh; popd
+	pushd ./chef_server && ./install_chef_server.sh; popd
 esac 
 
 
